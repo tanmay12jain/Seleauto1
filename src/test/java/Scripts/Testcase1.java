688be94c89882fe5d2370f667c4039b441620a8e
tanmay12jain <tanmayjn1997@gmail.com>
2024-01-20 20:57:21 +0530

    Architect American development

--- src/test/java/Scripts/Testcase1.java
+++ src/test/java/Scripts/Testcase1.java
@@ -32,4 +32,27 @@ public class Testcase1 extends BaseClass {
 		
 
 	}
+	@Test
+	public void tc2() {
+		SkillraryLoginPage s = new SkillraryLoginPage(driver);
+		s.gearsbutton();
+		s.skillrarydemoapplication();
+		
+		SkillraryDemoLoginPage sd = new SkillraryDemoLoginPage(driver);
+		driverutilies.switchtabs(driver);
+		driverutilies.mouseHover(driver, sd.getCourseBtn());
+		sd.seleniumtrainingtab();
+
+		AddtocartPage a = new AddtocartPage(driver);
+		driverutilies.dobleClick(driver, a.getAddbtn());
+		a.addtocartbtn();
+		driverutilies.alertpopup(driver);
+		
+		driverutilies.dobleClick(driver, a.getAddbtn());
+		a.addtocartbtn();
+		driverutilies.alertpopup(driver);
+		
+		
+
+	}
 }
